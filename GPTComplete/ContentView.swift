4bca42b8fa7e5d875f1c0ce48c6b96020135d4e4
Matt McMurry <matt@seis.com>
2023-03-19 16:11:49 -0600

    Initial Commit

---
+++ GPTComplete/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GPTComplete
+//
+//  Created by Matt McMurry on 3/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GPTComplete/GPTCompleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTCompleteApp.swift
+//  GPTComplete
+//
+//  Created by Matt McMurry on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct GPTCompleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
